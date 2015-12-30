0a16230f4bf710b4cebe6ae362a5d8b18d7a1854
njjneema <njjneema@gmail.com>
2015-12-30 14:00:17 -0800

    Update enroll.php

--- php/enroll.php
+++ php/enroll.php
@@ -36,7 +36,7 @@ $ui_id = $_SESSION["ui_id"];
             <div data-role="content"> 
         <br>
         <?php
-            $conn = mysql_connect('hkpowerzcom.ipagemysql.com', 'mariadb', 'password');
+            $conn = mysql_connect('neemajjcom.ipagemysql.com', 'mariadb', 'password');
             if (!$conn) {
                 die('Could not connect: ' . mysql_error());
             }
